615027e7e5242000a5f1cafb06a08e80e4ea6a4a
TheProShizer <marcin.siek20@gmail.com>
2023-01-25 17:56:30 +0100

    Aktualizacja

---
+++ app.js
@@ -0,0 +1,15 @@
+let typed = new Typed(".auto-type-text", {
+    strings: ["NETIZ931", 
+    "Good Seller", 
+    "łysy Pazdan", 
+    "JavaScript Developer", 
+    "https://dsc.gg/netiznsc", 
+    "Zwykły Bambik", 
+    "Rooonaldo", 
+    "TheProShizer#0001 to gigachad", 
+    "Kocham mame", 
+    "no elo elo",],
+    typeSpeed: 90,
+    backSpeed: 35,
+    loop: true
+}) 
